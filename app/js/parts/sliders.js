8b691abcf8d48b4b26b8352c312bd7bf35de802b
lorsalio7 <lorsalio7@gmail.com>
2024-11-19 14:30:02 +0500

    Добавить плагин Simplebar и убрать слайдер в секции Top of the month

--- app/js/parts/sliders.js
+++ app/js/parts/sliders.js
@@ -1,27 +1,29 @@
-let topOfTheMonthSlider = document.querySelector(".top-of-the-month-slider");
+// let topOfTheMonthSlider = document.querySelector(".top-of-the-month-slider");
 let popularPostsSlider = document.querySelector(".popular-posts-slider");
 
-if(topOfTheMonthSlider) {
-  topOfTheMonthSlider = new Splide(topOfTheMonthSlider, {
-    direction: 'ttb',
-    arrows: false,
-    pagination: false,
-    gap: 50,
-    wheel: true,
-    height: 460,
-    autoHeight: true,
-  });
+// if(topOfTheMonthSlider) {
+//   topOfTheMonthSlider = new Splide(topOfTheMonthSlider, {
+//     direction: 'ttb',
+//     arrows: false,
+//     pagination: false,
+//     gap: 50,
+//     wheel: true,
+//     height: 460,
+//     // autoHeight: true,
+//     perPage: 2,
+//     perMove: 2
+//   });
 
-  let topOfTheMonthSliderBar = topOfTheMonthSlider.root.querySelector(".top-of-the-month-slider__runner");
+//   let topOfTheMonthSliderBar = topOfTheMonthSlider.root.querySelector(".top-of-the-month-slider__runner");
 
-  topOfTheMonthSlider.on("mounted move", function() {
-    let end  = topOfTheMonthSlider.Components.Controller.getEnd() + 1;
-    let rate = Math.min( ( topOfTheMonthSlider.index + 1 ) / end, 1 );
-    topOfTheMonthSliderBar.style.height = String( 100 * rate ) + '%';
-  });
+//   topOfTheMonthSlider.on("mounted move", function() {
+//     let end  = topOfTheMonthSlider.Components.Controller.getEnd() + 1;
+//     let rate = Math.min( ( topOfTheMonthSlider.index + 1 ) / end, 1 );
+//     topOfTheMonthSliderBar.style.height = String( 100 * rate ) + '%';
+//   });
 
-  topOfTheMonthSlider.mount();
-}
+//   topOfTheMonthSlider.mount();
+// }
 
 if(popularPostsSlider) {
   popularPostsSlider = new Splide(popularPostsSlider, {
